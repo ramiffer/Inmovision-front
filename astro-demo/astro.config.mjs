2f1035aa604d4a98008c6dcd4f600d4f847591c7
Ramiro Fernandez <135069211+ramiffer@users.noreply.github.com>
2023-09-09 16:35:37 -0300

    Update astro.config.mjs

--- astro-demo/astro.config.mjs
+++ astro-demo/astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://ramiffer.github.io',
-  base: '/astro-demo/'
+  site: 'https://ramiffer.github.io'
 });
 
