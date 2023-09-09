910d760b8d8fb817e5679ee015a1bc14794dc4ce
Ramiro Fernandez <135069211+ramiffer@users.noreply.github.com>
2023-09-09 16:21:31 -0300

    Update astro.config.mjs

--- astro-demo/astro.config.mjs
+++ astro-demo/astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://github.com/ramiffer/Inmovision-front.git',
+  site: 'https://ramiffer.github.io',
   base: '/'
 });
 
