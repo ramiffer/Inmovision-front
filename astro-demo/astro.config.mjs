cf4d35b84def851f94b34179bae6f617d13b8a55
Ry-Ferreira <ryferreira1@outlook.es>
2023-09-13 20:08:48 -0300

    fix-hero-responsive_add-explore-section_crousel-not-work

--- astro-demo/astro.config.mjs
+++ astro-demo/astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://ramiffer.github.io'
+  integrations: [tailwind(), preact()],
+  site: 'https://ramiffer.github.io',
+  output: 'hybrid'
 });
-
