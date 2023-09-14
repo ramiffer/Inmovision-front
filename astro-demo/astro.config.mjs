a2401c3c9c6e7afd0cfa8513ca6b84c81f0ded9b
Ramiro Fernandez <135069211+ramiffer@users.noreply.github.com>
2023-09-14 16:42:17 -0300

    Update astro.config.mjs

--- astro-demo/astro.config.mjs
+++ astro-demo/astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import { node } from "@astro/types/node";
 
 import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
-  site: 'https://ramiffer.github.io',
-  output: 'hybrid'
-    adapter: node({
-    mode: 'standalone',
-  }),
+  site: 'https://ramiffer.github.io'
 });
