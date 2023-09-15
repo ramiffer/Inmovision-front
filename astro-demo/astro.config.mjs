7a817569037422a1b3057746971e692df44f2f67
ramiffer <contactoramiroff@gmail.com>
2023-09-14 21:04:52 -0300

    nodejs adapter

--- astro-demo/astro.config.mjs
+++ astro-demo/astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import preact from "@astrojs/preact";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), preact()],
   site: "https://ramiffer.github.io",
   output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
